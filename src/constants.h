2245201361b896e0fffae4eb99473ee46d61cbca
Pan Ziyue <random.rrr3@gmail.com>
2019-06-27 19:57:57 +0800

    added a few access point related command constants

--- src/constants.h
+++ src/constants.h
@@ -26,6 +26,10 @@
 static const struct mg_str COMMAND_NOP = MG_MK_STR("nop");
 static const struct mg_str COMMAND_VER = MG_MK_STR("ver");
 static const struct mg_str COMMAND_RST = MG_MK_STR("rst");
+
 static const struct mg_str COMMAND_LAP = MG_MK_STR("lap");
+static const struct mg_str COMMAND_CAP = MG_MK_STR("cap");
+static const struct mg_str COMMAND_SAP = MG_MK_STR("sap");
+static const struct mg_str COMMAND_DAP = MG_MK_STR("dap");
 
 #endif
