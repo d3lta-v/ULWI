b3889b24505a583dc55faec50793c033093c0bfb
Pan Ziyue <random.rrr3@gmail.com>
2019-08-22 11:32:45 +0800

    corrected some spacing issues

--- src/mqtt.c
+++ src/mqtt.c
@@ -1,14 +1,16 @@
 #include "mqtt.h"
 
-void mqtt_ev_handler(struct mg_connection *c, int ev, void *p,
-                       void *user_data) {
-  struct mg_mqtt_message *msg = (struct mg_mqtt_message *) p;
+void mqtt_ev_handler(struct mg_connection *c, int ev, void *p, void *user_data) {
+    struct mg_mqtt_message *msg = (struct mg_mqtt_message *) p;
 
-  if (ev == MG_EV_MQTT_CONNACK) {
-    LOG(LL_INFO, ("CONNACK: %d", msg->connack_ret_code));
-  } else if (ev == MG_EV_MQTT_SUBACK) {
-    LOG(LL_INFO, ("Subscription %u acknowledged", msg->message_id));
-  }
-  (void) user_data;
-  (void) c;
+    if (ev == MG_EV_MQTT_CONNACK)
+    {
+        LOG(LL_INFO, ("CONNACK: %d", msg->connack_ret_code));
+    }
+    else if (ev == MG_EV_MQTT_SUBACK)
+    {
+        LOG(LL_INFO, ("Subscription %u acknowledged", msg->message_id));
+    }
+    (void) user_data;
+    (void) c;
 }
